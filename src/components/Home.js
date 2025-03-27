0d8d38f0f65dd25d59daf65c78a41d9c80ec5843
N.Raju <151713861+NalagamdinniRaju@users.noreply.github.com>
2025-03-27 13:18:37 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -299,7 +299,8 @@ const Home = () => {
           </TechStack>
           <ButtonContainer>
             <Button 
-              href="https://drive.google.com/file/d/1HZu-XOFRsjm8p_e3wjSM749ENPd-BINw/view?usp=sharing"
+             // href="https://drive.google.com/file/d/1HZu-XOFRsjm8p_e3wjSM749ENPd-BINw/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Dmk9XfjM5tz4CZ5TTmDXb38AtaQ7sjKA/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
               primary
